2e25c34943ff762b09554d1f9e564c2e7c020d26
jaiswalabhay <117572181+jaiswalabhay@users.noreply.github.com>
2024-01-05 13:40:56 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,61 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    class Stack {
+    constructor() {
+        this.items = [];
+    }
     
+    // add element to the stack
+    add(element) {
+        return this.items.push(element);
+    }
+    
+    // remove element from the stack
+    remove() {
+        if(this.items.length > 0) {
+            return this.items.pop();
+        }
+    }
+    
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    
+    // check if the stack is empty
+    isEmpty(){
+       return this.items.length == 0;
+    }
+   
+    // the size of the stack
+    size(){
+        return this.items.length;
+    }
+ 
+    // empty the stack
+    clear(){
+        this.items = [];
+    }
+}
+
+let stack = new Stack();
+    for (let i = 0; i < s.length; i++) {
+          if(s[i]==('('||'['||'{' )
+             stack.add(s[i]);
+          else
+          {
+              if(stack.peek()==s[i])
+                  stack.remove();
+              else
+                  break;
+          }
+}
+
+    if(stack.isEmpty()==1)
+        return 1;
+    else
+        return 0;
 };
 
 module.exports = { isValid };
